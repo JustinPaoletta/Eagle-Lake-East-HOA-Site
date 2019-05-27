a6e8326e8818afc968e6eab9296e9488b1f918b4
Justin Paoletta <justinpaoletta@icloud.com>
2019-05-26 23:49:24 -0400

    Add files via upload

---
+++ New HOA Site/board.js
@@ -0,0 +1,14 @@
+$("#hamburger").click(function() {
+    $("#conportrait").toggle();
+    $("#conheader").toggle();
+
+});
+
+$(window).resize(function(){
+    
+    if($( window ).width()>799){
+        $("#conportrait").css("display", "none");
+        $("#conheader").css("display", "grid");
+    }
+    
+});
---
+++ New HOA Site/hoa.js
@@ -0,0 +1,5 @@
+$("#hamburger").click(function() {
+    $("#conportrait").toggle();
+    $("#conheader").toggle();
+
+});
